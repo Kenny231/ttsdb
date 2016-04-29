bc9aa552416e7f00fbe0b644ac5bd5f51b1d24cf
Kenny231 <K.bos1@student.han.nl>
2016-04-29 09:25:58 +0200

    Concept (design) home page.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<title>Bootstrap 101 Template</title>
 
 		<!-- Bootstrap -->
-		<link href="app/libs/bootstrap/css/boot-metro.min.css?<?php echo time(); ?>" rel="stylesheet">
+		<link href="app/libs/bootstrap/css/bootstrap.min.css?<?php echo time(); ?>" rel="stylesheet">
 		<link href="app/css/style.css?<?php echo time(); ?>" rel="stylesheet">
 		<link href="app/css/font-awesome.min.css?<?php echo time(); ?>" rel="stylesheet">
 		<link href="app/css/animate.min.css?<?php echo time(); ?>" rel="stylesheet">
