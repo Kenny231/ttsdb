c4ee3cfc30fffa9d538e4fc9172c5082e0725b99
Kenny231 <K.bos1@student.han.nl>
2016-05-09 15:34:52 +0200

    Toernooi form frontend klaar.

--- app/partials/Toernooi/Frontend/controllers/toernooi_controller.js
+++ app/partials/Toernooi/Frontend/controllers/toernooi_controller.js
@@ -1,5 +1,28 @@
 define(['app'], function (app) {
 	app.controller('ToernooiController', ['$scope', '$http', function ($scope, $http, $localStorage, accountService) {
     $scope.page = 1;
+		/*
+		 *Select data
+		*/
+		/* Geslacht */
+		$scope.select_geslacht = [
+			{name: 'Man',     value: 'm'},
+			{name: 'Vrouw',   value: 'v'},
+			{name: 'Gemengd', value: 'mv'}
+		];
+		$scope.geslacht = $scope.select_geslacht[0];
+		/* Toernooitype */
+		$scope.select_toernooi = [
+			'Familie',
+			'Ladder',
+			'Prestatie'
+		];
+		$scope.toernooitype = $scope.select_toernooi[0];
+		/* Enkel */
+		$scope.select_enkel = [
+			'Ja',
+			'Nee'
+		];
+		$scope.enkel = $scope.select_enkel[0];
 	}]);
 });
